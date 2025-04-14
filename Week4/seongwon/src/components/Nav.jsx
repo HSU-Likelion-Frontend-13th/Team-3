ae400aff9c914d6f1d5338ca5402337146287dd4
Seongwon20 <seangwon20@gmail.com>
2025-04-14 15:31:49 +0900

    feat: nav 스타일 적용

--- Week4/seongwon/src/components/Nav.jsx
+++ Week4/seongwon/src/components/Nav.jsx
@@ -2,11 +2,18 @@ import React from "react";
 import styled from "styled=components";
 
 const NavLayout=styled.nav`
-
+display: flex;
+  justify-content: center;
+  gap: 32px;
+  padding: 16px;
+  background-color: white;
 `;
 
 const NavItem=styled.a`
-
+ color: black;
+  text-decoration: none;
+  font-weight: bold;
+  cursor: pointer;
 `;
 
 export default function Nav() {
@@ -15,7 +22,7 @@ export default function Nav() {
         
           <NavItem href="#about">ABOUT</NavItem>
           <NavItem href="#hobby">HOBBY</NavItem>
-          <NavItem href="#">CONTACT</NavItem>
+          <NavItem href="#contact">CONTACT</NavItem>
         
       </NavLayout>
     );
