1c12c806f7b6330a0a206f9578c1a22726a1111e
Seongwon20 <seangwon20@gmail.com>
2025-04-14 13:49:02 +0900

    feat: App.js 작성

--- Week4/seongwon/src/App.js
+++ Week4/seongwon/src/App.js
@@ -1,1 +1,33 @@
+import React from "react";
+import globalStyles from "./styles/globalStyles"
+import Header from "./components/Header"
 import Nav from "./components/Nav";
+import Start from "./components/Nav";
+import About from "./components/Nav";
+import Hobby from "./components/Nav";
+
+function App() {
+    return (
+      <div className="App">
+        <globalStyle />
+        <header>
+          <Header />
+        </header>
+        <nav>
+            <Nav />
+        </nav>
+            <section>
+                <Start />
+            </section>
+            <section>
+                <About />
+            </section>
+            <section>
+                <Hobby />
+            </section>
+    
+      </div>
+    );
+  }
+
+  export default App;
