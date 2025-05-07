eb2d20b0ab37a1fe78baa17d178a2431f7c1ac27
Seongwon20 <seangwon20@gmail.com>
2025-05-07 13:09:34 +0900

    feat: editText 설정해서 수정할 수 있게

--- Week7/seongwon/src/components/main/todolist/TodoItem.jsx
+++ Week7/seongwon/src/components/main/todolist/TodoItem.jsx
@@ -5,19 +5,32 @@ import * as S from "../Main.style";
 
 export default function TodoItem({ todoText, todolist, setTodolist }) {
 
+  const [isEditing,setIsEditing]=useState(false); //초기설정은 false로로
+  const [editText, setEditText] = useState(todoText); 
+
+
   const deleteTodo = () => {
     setTodolist(todolist.filter((todo) => todo !== todoText));
   };
 
   const fixTodo=()=>{
-    console.log("클릭됌");
+    setIsEditing(true); //눌리면 수정 중으로 변경됌됌
   };
  
 
   return (
     <S.TodoItemLayout>
       <S.CheckboxAndTodoText>
+        {isEditing ?(
+          <input
+          type="text"
+          value={editText}
+          onChange={(e) => setEditText(e.target.value)}
+          />
+        ):(
+        
         <S.TodoText>{todoText}</S.TodoText>
+        )}
       </S.CheckboxAndTodoText>
       <S.DeleteButton onClick={deleteTodo}>삭제</S.DeleteButton>
       <S.EditButton onClick={fixTodo}>수정</S.EditButton>
